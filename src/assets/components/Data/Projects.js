55429ed58946b34fe6eb63b4721affbc53af0e11
Kiran2k2 <kiran.jena26022000@gmail.com>
2024-07-27 11:00:50 +0530

    portfolio

--- src/assets/components/Data/Projects.js
+++ src/assets/components/Data/Projects.js
@@ -1,75 +1,30 @@
 import { SiJavascript } from "react-icons/si";
 export let Project=[
-    {
-        id:1,
-        img:"src/assets/Screenshot (6).png",
-         title:"Amazon Clone",
-         sub:"Display the current weather conditions for the searched location",
+    
+     
+        {
+          id: 1,
+          img: 'src/assets/pokepon.jpg',
+          title: 'Pokemon Web',
+          sub: 'Explore and discover various Pokémon with detailed information and visuals.',
+          tech: 'ReactJS',
+          link: 'https://pokemon-show.vercel.app/',
+        },
+        {
+          id: 2,
+          img: 'src/assets/weather.jpg',
+          title: 'Weather API',
+          sub: 'Get real-time weather updates and forecasts for any location worldwide.',
+          tech: 'ReactJS',
+          link: 'https://weathera-36ab1d.netlify.app/',
+        },
+        {
+          id: 3,
+          img: 'src/assets/todoapp.jpg',
+          title: 'Todo App',
+          sub: 'Organize your tasks efficiently with this user-friendly to-do list application.',
+          tech: 'ReactJS',
+          link: 'https://todo-app-eight-smoky.vercel.app/',
+        },
+      ];
 
-         tech: "HTML,CSS"
-    },
-    {
-        id:2,
-        img:"src/assets/Screenshot (9).png",
-         title:"Weather API",
-         sub:"Display the current weather conditions for the searched location",
-         tech:"HTML,CSS,JS"
-    },
-    {
-        id:3,
-        img:"src/assets/Screenshot (8).png",
-         title:"Form validation",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "HTML,CSS,JS"
-    },
-    {
-        id:4,
-        img:"src/assets/Screenshot (10).png",
-         title:"Movie API",
-         sub:" Check out the most popular movies and imdb rating.",
-
-         tech: "React JS"
-    },
-    {
-        id:5,
-        img:"src/assets/Screenshot (7).png",
-         title:"Facebook singup page",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "HTML,CSS"
-    },
-    {
-        id:6,
-        img:"src/assets/Screenshot (11).png",
-         title:"Class UI",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "HTML,CSS"
-    },
-    {
-        id:7,
-        img:"src/assets/Screenshot (12).png",
-         title:"Kandalaa Clone",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "HTML,CSS"
-    },
-    {
-        id:8,
-        img:"src/assets/Screenshot (13).png",
-         title:"FAQ",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "ReactJS"
-    },
-    {
-        id:9,
-        img:"src/assets/Screenshot (11).png",
-         title:"Class UI",
-         sub:"Display the current weather conditions for the searched location",
-
-         tech: "HTML,CSS"
-    },
-
-]
